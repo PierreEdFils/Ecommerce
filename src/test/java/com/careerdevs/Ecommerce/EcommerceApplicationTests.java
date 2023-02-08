d27623846d43e3c2b5b4abc06185d8ce7e5ddcfa
Pierre Fils <epierrefils@gmail.com>
2023-02-07 23:44:49 -0500

    Initial commit

---
+++ src/test/java/com/careerdevs/Ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.Ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
